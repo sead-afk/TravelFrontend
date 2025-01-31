27d7c33cdfbcf5c40c076c3e5b16c7363c2727b0
sead-afk <61579298+sead-afk@users.noreply.github.com>
2025-01-31 15:18:19 +0100

    Add vendor folder to the repository

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'da4c87da951566fba07e76a7cc4fafab64d8d409',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'da4c87da951566fba07e76a7cc4fafab64d8d409',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mikecao/flight' => array(
+            'pretty_version' => 'v3.13.0',
+            'version' => '3.13.0.0',
+            'reference' => '1307e8a39d89fadba69d0c2dad53b6e0da83fd96',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mikecao/flight',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
