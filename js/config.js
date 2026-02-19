ac49f95047b0c8f97bcd2dab38978ce63d856b18
sead-afk <sead.gacanovic@stu.ibu.edu.ba>
2026-02-19 16:54:08 +0100

    config.js url fix

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.API_BASE_URL =
   location.hostname === "localhost"
     ? "http://localhost:8080"
-    : "https://travelapp-backend-vatx.onrender.com";
+    : "https://travelapp-backend-frod.onrender.com";
